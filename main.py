25519140e07a70cebcc57ac1778ca4eccedc6c5f
FilipSenekovic <filip.senekovic@gmail.com>
2022-05-11 22:12:56 +0200

    Test commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def test123():
+def test123456():
     print("testni print.\n")
 
 
