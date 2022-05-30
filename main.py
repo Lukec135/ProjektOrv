8b467255a15ddfa181c07b543443b194dc728709
Mernik <mernik.luka1@gmail.com>
2022-05-30 19:06:26 +0200

    Find and detect face

--- main.py
+++ main.py
@@ -11,16 +11,61 @@ import requests
 import json
 
 
+#######################################
+#numpy~=1.22.4
+#opencv-python~=4.5.5.64
+#opencv-contrib-python~=4.5.5.64
 
-app = Flask(__name__)
+import re
+
+import numpy as np
+
+import cv2
+
+import glob
 
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+
+face_detector_path = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(face_detector_path)  # POLJUBNI DETEKTOR
+
+model = cv2.face.LBPHFaceRecognizer_create()  # Local Binary Patterns Histograms #LBP   <---- !!!
+
+face_db = []
+faces = []
 
 
+def detect_face(img_path1):
+    img1 = cv2.imread(img_path1)
+
+    detected_faces = faceCascade.detectMultiScale(img1, 1.3, 5)
+    x, y, w, h = detected_faces[0]  # focus on the 1st face in the image
+
+    img1 = img1[y:y + h, x:x + w]  # focus on the detected area
+    img1 = cv2.resize(img1, (224, 224))
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+    return img1
+
+
+def findFace(target_file):
+    img2 = detect_face(target_file)
+    # print(img.shape)
+
+    idx, confidence = model.predict(img2)
+
+
+    print("Confidence: ", round(confidence, 2))
+    print("Path: ", face_db[idx])
+    match_path = face_db[idx]
+    #match_name = re.sub(r'^.*?\\', '', match_path)
+
+    print("Name: ", match_path)
+    #plt.show()
+    return match_path
+
+
+##############################################################################
 
 @app.route('/vse_slike', methods=['POST'])
 def vse_slike():
