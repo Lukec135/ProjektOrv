9da9b3b10049ef1478ecfa809bc3a49ffd329526
Ziga Adam <ziga.adam@gmail.com>
2022-05-30 17:56:44 +0200

    Ziga 3. commit: - dodajanje slik - pregled slik

--- main.py
+++ main.py
@@ -1,27 +1,71 @@
-import numpy as np
-import cv2
+import base64
+import io
+import json
+import os
+from tkinter import Image
 
-import numpy as np
-import matplotlib.pyplot as plt
+import requests as requests
+from flask import Flask
+from flask import request, jsonify
+
+# import io
+# import base64
+# from PIL import Image
 
 
-def Prepoznava_obraza():
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
-    #save the image(i) in the same directory
-    img = cv2.imread("Slike/jozek.jpg")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        cv2.imwrite('Obraz/'+str(w) + str(h) + '_faces.jpg', roi_color)
-    #    eyes = eye_cascade.detectMultiScale(roi_gray)
-    #for (ex,ey,ew,eh) in eyes:
-    #        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+app = Flask(__name__)
 
-Prepoznava_obraza()
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "Mernik in Adam")
+    return "Hello {}!".format(name)
+
+
+@app.route('/dodaj', methods=['POST'])
+def dodaj_sliko():
+    if not request.json or not 'ime' in request.json:
+        return 400
+
+    ime = request.json['ime']
+    slika = request.json['slika']
+
+    # with open("imageToSave.png", "wb") as fh:
+    #    fh.write(base64.decodebytes(img_data))
+
+    # img = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
+    # img.save("slike\\" + ime + '.png')
+
+    return ("DELA ime: " + ime + " slika: " + slika).jsonify
+
+@app.route('/vse_slike', methods=['POST'])
+def vse_slike():
+
+    url = 'https://silent-eye-350012.oa.r.appspot.com/images/listAPI'
+    response = requests.post(url)
+
+    try:
+        data = response.json()
+
+        c = int(0)
+        for i in data['images']:
+            ime = i['ime']
+            slika = i['slika']
+
+            # Assuming base64_str is the string value without 'data:image/jpeg;base64,'
+            img4 = Image.open(io.BytesIO(base64.decodebytes(bytes(slika, "utf-8"))))
+            img4.save('slike/' + ime + ' ' + str(c) + '.png')
+            c = c+1
+
+        _, _, files = next(os.walk("slike"))
+        file_count = len(files)
+        #return str(file_count)
+        value = {
+            "velikost": str(file_count)
+        }
+        return json.dumps(value)
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
